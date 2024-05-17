25719d7da9cd02c38e67fd88a7b64ed679626955
Trish <Trish@LAPTOP-HKCBLEKG>
2024-05-18 00:36:15 +0800

    Add project files.

---
+++ Module04Navigation/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Module04Navigation
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public async void CarPage(Object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new CarouselPage1());
+        }
+        public async void TabPage(Object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new TabbedPage1());
+        }
+    }
+}
---
+++ Module04Navigation/TabbedPage1.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Module04Navigation
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TabbedPage1 : TabbedPage
+    {
+        public TabbedPage1()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnSubmitClicked(object sender, EventArgs e)
+        {
+            string some = Entry.Text;
+
+            await DisplayAlert("Joke Submitted", some, "OK");
+        }
+    }
+}
